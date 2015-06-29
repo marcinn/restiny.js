fcef3a44880e59193d00a189a57c3080d917187b
Marcin Nowak <marcin.j.nowak@gmail.com>
2015-06-29 19:20:05 +0200

    angular support

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ hour, not tested, and not reliable. But extremely simple to use :)
 ## Usage
 
 
-An example of hitting GitHub API and fetching my followers.
+An example of hitting GitHub API and fetching my followers (using jQuery).
 
 ```javascript
 
@@ -23,6 +23,26 @@ github.get('users/marcinn').then(function(user) {
 });
 
 ```
+
+An example of hitting GitHub API and fetching my followers (using
+Angular).
+
+```javascript
+
+angular.module('restinyDemo', [
+    'restiny'
+])
+.run(function(restiny) {
+    var github = restiny.api('https://api.github.com');
+
+    github.get('users/marcinn').then(function(resp) {
+        github.get(resp.data.followers_url).then(function(resp) {
+            console.log(resp.data);
+        });
+    });
+});
+```
+
 
 ## Author and license
 
--- restiny.js
+++ restiny.js
@@ -6,11 +6,54 @@
  *
  */
 
-var Restiny = function(url, config) {
+var jqueryAdapter = function(restiny) {
+    var makeResponseObject = function(jqXhr, data) {
+        var headers = {},
+            headersList = jqXhr.getAllResponseHeaders().split('\r\n');
+        $.each(headersList, function(i, headerStr) {
+            var headerTuple = headerStr.split(': ');
+            if(headerTuple[0]) {
+                headers[headerTuple[0].toLowerCase()] = headerTuple[1];
+            }
+        });
+        return {
+            status: jqXhr.status,
+            statusText: jqXhr.statusText,
+            headers: headers,
+            data: data 
+        };
+    };
+
+    this.doMethod = function(type, url, data, headers) {
+        var dfr = $.Deferred(),
+            completeHeaders = $.extend({}, restiny._defaultHeaders, headers);
+
+        $.ajax({
+            url: url,
+            type: type,
+            dataType: type=='GET' ? null : 'json',
+            contentType: type=='GET' ? null : 'application/json',
+            headers: completeHeaders,
+            data: typeof(data)!=='undefined' && data!==null ? (type=='GET' ? data : JSON.stringify(data)) : null
+        }).done(function(respData,respStatus,jqXhr) {
+            if(jqXhr.status >= 200 && jqXhr.status<300) {
+                dfr.resolve(respData, makeResponseObject(jqXhr, respData));
+            } else {
+                dfr.reject(makeResponseObject(jqXhr));
+            }
+        });
+        return dfr.promise();
+    }
+};
+
+
+var Restiny = function(url, config, adapter) {
     var absoluteUrlPattern = /^https?:\/\//i,
-        self=this;
+        self=this,
+        adapter = adapter || jqueryAdapter;
 
     this._config = config || {};
+    this._adapter = new adapter(this);
     this._defaultHeaders = this._config.headers || {};
     this._url = url;
 
@@ -27,40 +70,8 @@ var Restiny = function(url, config) {
         }
     }
 
-    function makeResponseObject(jqXhr, data) {
-        var headers = {},
-            headersList = jqXhr.getAllResponseHeaders().split('\r\n');
-        $.each(headersList, function(i, headerStr) {
-            var headerTuple = headerStr.split(': ');
-            if(headerTuple[0]) {
-                headers[headerTuple[0].toLowerCase()] = headerTuple[1];
-            }
-        });
-        return {
-            status: jqXhr.status,
-            statusText: jqXhr.statusText,
-            headers: headers,
-            data: data 
-        };
-    }
-
     function doMethod(type, resource, data, headers) {
-        var dfr = $.Deferred(),
-            completeHeaders = $.extend({}, self._defaultHeaders, headers);
-
-        $.ajax({
-            url: resourceToUrl(resource),
-            type: type,
-            dataType: type=='GET' ? null : 'json',
-            contentType: type=='GET' ? null : 'application/json',
-            headers: completeHeaders,
-            data: typeof(data)!=='undefined' && data!==null ? (type=='GET' ? data : JSON.stringify(data)) : null
-        }).then(function(respData,respStatus,jqXhr) {
-            dfr.resolve(respData, makeResponseObject(jqXhr, respData));
-        }, function(jqXhr) {
-            dfr.reject(makeResponseObject(jqXhr));
-        });
-        return dfr.promise();
+        return self._adapter.doMethod(type, resourceToUrl(resource), data, headers);
     };
 
     
@@ -83,5 +94,34 @@ var Restiny = function(url, config) {
     this['delete'] = function(resource, headers) {
         return doMethod('DELETE', resource, null, headers);
     }
-}
+};
 
+angular ? angular.module('restiny', [])
+.factory('restiny', ['$http', '$q', function($http, $q) {
+    var angularRestinyAdapter = function(restiny) {
+        this.doMethod = function(type, url, data, headers) {
+            var dfr = $q.defer(),
+                completeHeaders = angular.extend({}, restiny._defaultHeaders, headers),
+                params = type.toLowerCase()=='get';
+
+            $http({
+                method: type,
+                url: url,
+                headers: completeHeaders,
+                data: params ? null : data,
+                params: params ? data : null
+            }).then(function(resp) {
+                dfr.resolve(resp);
+            }, function(resp) {
+                dfr.reject(resp);
+            });
+            return dfr.promise;
+        }
+    };
+    return {
+        api: function(path, config) {
+            return new Restiny(path, config, angularRestinyAdapter);
+        }
+    };
+}]) : null
+;
